f200803e0b95d88d2f1e1124a0931364a3e06de2
trungtashi99 <trungtashi@gmai.com>
2022-08-16 16:49:16 +0700

    Thực hành triển khai interface 'Comparable' cho các lớp hình học

---
+++ src/Circle.java
@@ -0,0 +1,49 @@
+public class Circle {
+    private double radius;
+    private String color;
+    private boolean filled;
+    public Circle(double radius) {
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public Circle() {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
